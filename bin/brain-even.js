4050dd1c943b847bc0fe9decacd545bb86f7e0f8
Miyuki <7001vlbl@bk.ru>
2024-01-18 13:28:42 +0300

    4 шаг осталось 5-тая задача

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,40 @@
-const evenOrNot = () => {
-  let randomNumber = Math.floor(Math.random() * 20) + 2;
+import readlineSync from 'readline-sync';
+import { greeting } from '../src/cli.js';
 
-console.log(randomNumber)
+const name = greeting();
+let gameOver = false; //отслеживание состояния игры
+let win = false; //отслеживание состояния игры
+let correctCount = 0;
+const random = () => {
+  if (gameOver) { // Если игра завершилась то не запускать новую
+    return;
+  }
+
+  const randomNumber = Math.floor(Math.random() * 20) + 1;
+  console.log('Question: ' + randomNumber); 
+  const correctAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
+  const answer = readlineSync.question('Your answer?: ');
+
+  if (answer.toLowerCase() === correctAnswer) {
+    console.log('Correct!');
+    correctCount += 1
+  } else if (answer != correctAnswer){ 
+   console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+   console.log(`Let's try again, ${name}` + '!');
+   gameOver = true; //чтобы игра больше не запускалась
+  } else {
+    win = true;
+  }
+ }
+
+for (let i = 0; i < 3; i += 1) {
+  random();
 }
-evenOrNot()
+
+if (correctCount === 3) {
+  win = true;
+}
+
+if(win) {
+  console.log('Congratulations, ' + name + '!')
+}
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,3 @@
 import { greeting } from '../src/cli.js';
 
 greeting()
-console.log('Answer "yes" if the number is even, otherwise answer "no".')

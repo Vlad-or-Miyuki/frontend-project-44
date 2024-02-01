82ca24fc4b8abcc52a05ca421359d565ce2c969b
Miyuki <7001vlbl@bk.ru>
2024-02-01 09:26:56 +0300

    Revert "fixed greeting"
    This reverts commit c5ac3ab76f3c9566d22b0093c0d6171128639656.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,2 @@
 #!/usr/bin/env node
-import greeting from "../src/cli.js";
-
-const name = greeting();
-
-const welcome = (name) => {
-  console.log('Welcome to the Brain Games!');
-  console.log(name);
-};
-
-welcome(name);
+console.log('Welcome to the Brain Games!');

d792b16008783f5e8d312ba5926ccfe145fde4c5
Miyuki <7001vlbl@bk.ru>
2024-02-01 10:49:04 +0300

    WIP(brain-games.js fixing)

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
-
-const userName = greeting();
 
 const welcome = () => {
   console.log('Welcome to the Brain Games!');
   console.log(userName)
 };
-
-export default welcome;
